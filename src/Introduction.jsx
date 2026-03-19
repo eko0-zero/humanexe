408327a6453a5ca9db76617afb12e229afbae722
eko0-zero <eko00.5177@gmail.com>
2026-03-19 02:40:05 +0100

    add animation intro

--- src/Introduction.jsx
+++ src/Introduction.jsx
@@ -3,6 +3,11 @@ import arrow from "./assets/img/svg/arrow.svg";
 import gsap from "gsap";
 
 export default function Introduction({ onEnter }) {
+  const titleRef = useRef(null);
+  const headingRef = useRef(null);
+  const contextRef = useRef(null);
+  const ctaRef = useRef(null);
+
   useEffect(() => {
     const preventScroll = (e) => e.preventDefault();
 
@@ -110,23 +115,43 @@ export default function Introduction({ onEnter }) {
     };
   }, [onEnter]);
 
+  useEffect(() => {
+    const tl = gsap.timeline({
+      defaults: { duration: 1.3, ease: "power2.out" },
+    });
+    tl.to([headingRef.current, contextRef.current], { opacity: 1, y: 0 });
+    tl.to(ctaRef.current, { opacity: 1, y: 0 }, "-=0.6");
+  }, []);
+
   return (
     <main
-      className="relative h-[100vh] w-[100vw] flex cursor-pointer"
+      className="relative h-[100vh] w-[100vw] flex cursor-pointer overflow-hidden"
       onClick={onEnter}
     >
-      <div className="title-box flex items-center justify-between w-full px-[5%] max-[1000px]:flex-col max-[1000px]:justify-center max-[1000px]:items-baseline max-[1000px]:gap-[2vh]">
-        <h1 className="title font-host font-regular text-[6rem] max-[1100px]:text-[5.2rem] min-[1900px]:text-[10rem] text-black">
+      <div
+        ref={titleRef}
+        className="title-box flex items-center justify-between w-full px-[5%] max-[1000px]:flex-col max-[1000px]:justify-center max-[1000px]:items-baseline max-[1000px]:gap-[2vh]"
+      >
+        <h1
+          ref={headingRef}
+          className="title font-host font-regular text-[6rem] max-[1100px]:text-[5.2rem] min-[1900px]:text-[10rem] text-black opacity-0 translate-y-10"
+        >
           Human.exe
         </h1>
-        <p className="context font-host font-light italic text-[1.3rem] max-[1100px]:text-[1rem] max-[1000px]:w-[65vw] min-[2500px]:text-[1.6rem] text-black w-[45vw]">
+        <p
+          ref={contextRef}
+          className="context font-host font-light italic text-[1.3rem] max-[1100px]:text-[1rem] max-[1000px]:w-[65vw] min-[2500px]:text-[1.6rem] text-black w-[45vw] opacity-0 translate-y-10"
+        >
           text intro Lorem ipsum dolor sit amet, consectetur adipiscing elit,
           sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
           enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
           ut aliquip ex ea commodo consequat.
         </p>
       </div>
-      <div className="call-to-action flex flex-col items-center justify-center absolute bottom-10 left-1/2 -translate-x-1/2">
+      <div
+        className="call-to-action flex flex-col items-center justify-center absolute bottom-10 left-1/2 -translate-x-1/2 opacity-0 translate-y-10"
+        ref={ctaRef}
+      >
         <p className="scroll-or-click font-host font-light italic text-[1.8rem] max-[1000px]:text-[1.2rem] text-black text-center">
           scroll or click
         </p>
